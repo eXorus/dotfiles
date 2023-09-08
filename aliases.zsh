92ece46352414c7fe145eb462e1eee322960f0c8
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-08 18:33:16 +0200

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,40 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias code="cd $HOME/Code"
+alias herd="cd $HOME/Herd"
+
+# Laravel
+alias a="php artisan"
+alias fresh="php artisan migrate:fresh --seed"
+alias tinker="php artisan tinker"
+alias seed="php artisan db:seed"
+alias serve="php artisan serve"
+
+# PHP
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias watch="npm run watch"
+
+# Docker
+alias docker-composer="docker-compose"
+
+# Git
+alias gs="git status"
+alias gaa="git add ."
+alias gcb="git checkout -b"
+alias gco="git checkout"
+alias gc="git commit -m"
+alias gpf="git push --force-with-lease"
+alias diff="git diff"
+alias nah="git reset --hard;git clean -df;"
+alias gl="git pull"
+alias gp="git push"
