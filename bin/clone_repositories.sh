4ff99f2b823a228f68edf9de4d8f0138685d4b98
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-14 16:02:40 +0200

    Create clone_repositories.sh

---
+++ bin/clone_repositories.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Cloning repositories..."
+
+CODE=$HOME/Code
+SITES=$HOME/Herd
+
+# Sites
+git clone git@github.com:eXorus/codeception.cloud.git $SITES/codeception.cloud
+git clone git@github.com:php-mime-mail-parser/emailparser-tools.git $SITES/emailparser-tools
+
+# Code
+git clone git@github.com:php-mime-mail-parser/php-mime-mail-parser.git $CODE/pmmp
+git clone git@github.com:OpenClassrooms/WebAcceptanceTests.git $CODE/wat
