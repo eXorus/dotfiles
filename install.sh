fccf2a3b4110137bef22e457ade2066d62b276c6
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-14 12:33:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,20 @@ abort() {
   exit 1
 }
 
+# string formatters
+if [[ -t 1 ]]
+then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
 ring_bell() {
   # Use the shell's audible bell.
   if [[ -t 1 ]]
