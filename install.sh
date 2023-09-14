0e245a9904f9f0b82857694f0f18d38ee38843f5
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-14 12:31:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,39 @@ abort() {
   exit 1
 }
 
+ring_bell() {
+  # Use the shell's audible bell.
+  if [[ -t 1 ]]
+  then
+    printf "\a"
+  fi
+}
+
+wait_for_user() {
+  local c
+  echo
+  echo "Press ${tty_bold}RETURN${tty_reset}/${tty_bold}ENTER${tty_reset} to continue or any other key to abort:"
+  getc c
+  # we test for \r and \n because some stuff does \r instead
+  if ! [[ "${c}" == $'\r' || "${c}" == $'\n' ]]
+  then
+    exit 1
+  fi
+}
+
 # First check OS.
 OS="$(uname)"
-if [[ "${OS}" == "Linux" ]]
+if [[ "${OS}" == "Darwin" ]]
 then
-  abort "Homebrew is supported on linux."
-elif [[ "${OS}" == "Darwin" ]]
-then
-  abort "Homebrew is supported on macOS."
+  INSTALL_ON_MACOS=1
 else
-  abort "Homebrew is only supported on macOS and Linux."
+  abort "Install is only supported on macOS."
 fi
+
+ring_bell
+wait_for_user
+
+//generate SSH Key
+//wait for key copied in github
+//clone dotfiles repository with git
+//start fresh
