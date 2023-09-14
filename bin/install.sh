b241630e4e228ba5c0e535b85920f1af1714607b
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-14 15:52:47 +0200

    Create install.sh

---
+++ bin/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ring_bell() {
+  # Use the shell's audible bell.
+  if [[ -t 1 ]]
+  then
+    printf "\a"
+  fi
+}
+
+ask() {
+    ring_bell
+    read -p "$1 [Yes/No]: " response
+    case "$response" in
+        [yY]|[yY][eE][sS]) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+if ask "Should I generate a new SSH Key for GitHub?"; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/exorus/dotfiles/HEAD/generate_ssh_key.sh)"
+fi
