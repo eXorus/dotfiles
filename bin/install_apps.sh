be8e2e9af1c7545c749552b7db1a15aba5606742
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-15 16:38:34 +0200

    Update install_apps.sh

--- bin/install_apps.sh
+++ bin/install_apps.sh
@@ -3,3 +3,7 @@
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle --file $HOME/.dotfiles/Brewfile
+
+
+
+echo "memory_limit=-1" >> /Users/$USER/Library/Application\ Support/Herd/config/php/82/php.ini
