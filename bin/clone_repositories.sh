3751316bcd4727c7fbe367300278bcf39307594a
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-15 15:24:42 +0200

    Update clone_repositories.sh

--- bin/clone_repositories.sh
+++ bin/clone_repositories.sh
@@ -13,8 +13,16 @@ mkdir $HOME/Herd
 
 # Sites
 git clone git@github.com:eXorus/codeception.cloud.git $SITES/codeception.cloud
-git clone git@github.com:php-mime-mail-parser/emailparser-tools.git $SITES/emailparser-tools
+git clone git@github.com:php-mime-mail-parser/emailparser-tools.git $SITES/emailparser.tools
+git clone git@github.com:simpleit/SdZv4.git $SITES/openclassrooms.com
 
 # Code
 git clone git@github.com:php-mime-mail-parser/php-mime-mail-parser.git $CODE/pmmp
 git clone git@github.com:OpenClassrooms/WebAcceptanceTests.git $CODE/wat
+git clone git@github.com:OpenClassrooms/UtilityScripts.git $CODE/UtilityScripts
+git clone git@github.com:OpenClassrooms/ios.git $CODE/ios
+
+
+
+
+ 
