45138b91eb712a7514b07998f066e67893251dc0
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-18 15:46:35 +0200

    Create paths.zsh

---
+++ paths.zsh
@@ -0,0 +1,11 @@
+# Load Composer tools
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+
+# Load UtilityScripts AWS
+export PATH="$HOME/Code/UtilityScripts/AWS/bin:$PATH"
+
+# Load Node global installed binaries
+export PATH="$HOME/.node/bin:$PATH"
+
+# Use project-specific binaries before global ones
+export PATH="node_modules/.bin:vendor/bin:$PATH"
