a66462f68dc0ed06e71cb63413413366ea2c434e
Vincent Dauce <eXorus@users.noreply.github.com>
2023-09-18 12:34:33 +0200

    Update install_apps.sh

--- bin/install_apps.sh
+++ bin/install_apps.sh
@@ -8,3 +8,6 @@ brew bundle --file $HOME/.dotfiles/Brewfile
 
 git config --global push.default current
 echo "memory_limit=-1" >> /Users/$USER/Library/Application\ Support/Herd/config/php/82/php.ini
+
+open --background -a Docker
+open --background -a Proxyman
