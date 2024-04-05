95bdc02393e356d502f3659a6bc60c8b13db43e4
Vincent Dauce <eXorus@users.noreply.github.com>
2024-04-05 16:21:02 +0200

    Update paths.zsh

--- paths.zsh
+++ paths.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Load UtilityScripts AWS
 export PATH="$HOME/Code/UtilityScripts/AWS/bin:$PATH"
 
+# Load WAT
+export PATH="$HOME/Code/wat/bin:$PATH"
+
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
